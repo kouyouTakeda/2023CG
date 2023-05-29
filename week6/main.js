686017adf3ef0a5ea633a5b4ae9e0655afd23e55
kouyouTakeda <kouyou.pc1202@gmail.com>
2023-05-29 22:24:11 +0900

    add particle

--- week6/main.js
+++ week6/main.js
@@ -17,6 +17,8 @@ camera.position.set(2, 5, 15);
 // レンダラー
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+// 影づけ
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 // カメラのコントロール
@@ -25,13 +27,15 @@ controls.update();
 
 // ライト
 const directionalLight = new THREE.DirectionalLight(0xffffff);
-directionalLight.position.set(5, 100, 100);
+directionalLight.position.set(100, 100, 100);
+// 影を有効
+directionalLight.castShadow = true;
 scene.add(directionalLight);
-// const helper = new THREE.DirectionalLightHelper(directionalLight, 5);
-// scene.add(helper);
+const helper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(helper);
 
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(ambientLight);
+// const ambientLight = new THREE.AmbientLight(0xffffff);
+// scene.add(ambientLight);
 
 // 球体
 const sphereGeometry = new THREE.SphereGeometry(5, 32, 16);
@@ -39,48 +43,81 @@ const textureUrl = new URL(
   "./texture/brick_wall_001_4k.blend/textures/brick_wall_001_diffuse_4k.jpg",
   import.meta.url
 ).href;
-const texture = new THREE.TextureLoader().load(
-  "/texture/brick_wall_001_4k.blend/textures/brick_wall_001_diffuse_4k.jpg"
-);
-const sphereMaterial = new THREE.MeshBasicMaterial({ map: texture });
+const texture = new THREE.TextureLoader().load(textureUrl);
+const sphereMaterial = new THREE.MeshStandardMaterial({ map: texture });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.castShadow = true;
 sphere.position.set(0, 10, -5);
 scene.add(sphere);
 
-const sphereMaterial2 = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const sphere2 = new THREE.Mesh(sphereGeometry, sphereMaterial2);
-sphere2.position.set(10, 10, -5);
-scene.add(sphere2);
+// 床
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1000, 1000),
+  new THREE.MeshStandardMaterial({ color: 0x000000 })
+);
+plane.rotation.x = -0.5 * Math.PI;
+plane.receiveShadow = true;
+scene.add(plane);
 
-const sphere3 = new THREE.Mesh(sphereGeometry, sphereMaterial2);
-sphere3.position.set(-10, -10, -5);
-scene.add(sphere3);
+// パーティクル（星）
+// 頂点座標
+const vertices = [];
+const colors = [];
+for (let i = 0; i < 3000; i++) {
+  const x = 1000 * (Math.random() - 0.5);
+  const y = 500 * Math.random();
+  const z = 1000 * (Math.random() - 0.5);
+
+  // color 0〜1
+  colors.push(Math.random(), Math.random(), Math.random());
+  vertices.push(x, y, z);
+}
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute(
+  "position",
+  new THREE.Float32BufferAttribute(vertices, 3)
+);
+particlesGeometry.setAttribute(
+  "color",
+  new THREE.Float32BufferAttribute(colors, 3)
+);
+const particlesMaterial = new THREE.PointsMaterial({
+  size: 0.5,
+  vertexColors: true,
+});
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
 // 3DModel
 const loader = new FBXLoader();
-loader.load(
+const carUrl = new URL(
   "/3dModel/Jeep_Renegade_2016_fbx/Jeep_Renegade_2016.fbx",
-  function (object) {
-    object.traverse(function (child) {
-      if (child.isMesh) {
-        new THREE.TextureLoader().load(
-          "/3dModel/Jeep_Renegade_2016_fbx/Jeep_Renegade_2016/car_jeep_ren.jpg",
-          (texture) => {
-            child.material.map = texture;
-            child.material.needsupdate = true;
-          }
-        );
-        child.castShadow = true;
-        child.receiveShadow = true;
-      }
-    });
-    object.scale.set(2, 2, 2);
-    scene.add(object);
-  }
-);
+  import.meta.url
+).href;
+loader.load(carUrl, function (object) {
+  object.traverse(function (child) {
+    if (child.isMesh) {
+      const carTexture = new URL(
+        "/3dModel/Jeep_Renegade_2016_fbx/Jeep_Renegade_2016/car_jeep_ren.jpg",
+        import.meta.url
+      ).href;
+      new THREE.TextureLoader().load(carTexture, (texture) => {
+        child.material.map = texture;
+        child.material.needsupdate = true;
+      });
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  object.scale.set(2, 2, 2);
+  scene.add(object);
+});
 
 // アニメーション
 const animate = () => {
+  // 回転
+  sphere.rotation.y += 0.005;
+
   requestAnimationFrame(animate);
   controls.update();
   renderer.render(scene, camera);
